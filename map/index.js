66098a79a088d97c17d5133d65903dc8e80bc60c
Hugo <114680022+hugorodriv@users.noreply.github.com>
2024-04-22 12:17:28 +0100

    map initial version

---
+++ map/index.js
@@ -0,0 +1,120 @@
+var countryVisits = {};
+var max_value;
+var popup;
+var last_country_popup;
+var geoJson;
+
+var map = L.map('map', { 
+    attributionControl:false, 
+    maxBoundsViscosity: 0.3,
+    minZoom: 2,
+    maxZoom: 5,
+    autoPan: false,
+    maxBounds: [[-90,-180],   [90,180]],
+    keyboard:false}).setView([30, 7], 2);
+
+function getColor(id){
+    var maxLightness = 75
+    var minLightness = 35
+    var percent = countryVisits[id]/max_value
+    var w1 = percent
+    var w2 = 1 - w1
+    var interpolated = (minLightness * w1) + (maxLightness * w2)
+    interpolated = interpolated || 90
+    return `hsl(200, 100%, ${interpolated}%)`;
+}
+
+function getPopupText(e){
+    var num = countryVisits[e.target.feature.id];
+    if (num === undefined){
+        num = 0;
+    }
+    return String(e.target.feature.properties.name + " : " + num)
+}
+
+function highlight(e){
+    var layer = e.target;
+    var current_country = e.target.feature.id;
+    if (current_country != last_country_popup){
+        popup = L.popup({
+            closeButton: false        
+        })
+            .setLatLng(e.latlng)
+            .setContent(getPopupText(e))
+            .openOn(map);
+    }
+
+    last_country_popup = e.target.feature.id;
+    layer.setStyle({fillOpacity: 0.8});
+    layer.bringToFront();
+}
+
+function onEachFeature(feature, layer) {
+    if("ATA" === layer.feature.id){     //remove antartica
+        layer.remove();
+    }
+    layer.on({
+        click: highlight,
+        mouseover: highlight,
+        mouseout: resetHighlight,
+    });
+}
+function resetHighlight(e) {
+    e.sourceTarget.setStyle({fillOpacity:0.4});
+    popup.remove();
+    last_country_popup = null;
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.id),
+        weight: 0.2,
+        color: 'grey',
+        fillOpacity: 0.4
+    };
+}
+
+
+async function fetchCountryData() {
+    return await fetch('countries.json', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+        .then((response)=>response.json())
+        .then((responseJson)=>{return responseJson});
+}
+
+function fetchGeoJson(){
+    fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+        fillMap(data);
+    })
+    .catch(error => {
+        console.error('Error fetching GeoJSON:', error);
+    });
+}
+async function fillMap(response){
+    geoJson = L.geoJson(response, {style: style(response)}).addTo(map)
+    geoJson.eachLayer(function (layer) {
+        onEachFeature(layer.feature, layer);
+    });
+
+    countryVisits = await fetchCountryData();
+    max_value = Math.max(...Object.values(countryVisits));
+    geoJson.eachLayer(function (layer) {
+        layer.setStyle(style(layer.feature));
+    });
+    // map.fitWorld();
+}
+
+
+fetchGeoJson();
