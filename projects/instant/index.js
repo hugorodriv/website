38fd4deafb9b7e79c0dec4da66f5d5ca2b192fa7
Hugo <114680022+hugorodriv@users.noreply.github.com>
2024-08-14 22:53:03 +0200

    country SSE highlight :)

--- projects/instant/index.js
+++ projects/instant/index.js
@@ -15,38 +15,12 @@ var map = L.map("map", {
     keyboard: false
 }).setView([40, 0], 1.5);
 
-
-
-function getColor(id) {
-    let a = 25
-    let b = 60
-
-    let percent = countryVisits[id] / max_value
-    let interpolated = (b * percent) + (a * (1 - percent))
-
-    interpolated = interpolated || 20
-    return `hsl(210, 100%, ${interpolated}%)`;
-}
-
 function getPopupText(e) {
     var num = countryVisits[e.target.feature.id];
     if (num === undefined) {
         num = 0;
     }
-    return String(e.target.feature.properties.name + " : " + num)
-}
-
-function highlight(e) {
-    var layer = e.target;
-    popup = L.popup({
-        closeButton: false,
-        autoPan: false
-    })
-        .setLatLng(e.latlng)
-        .setContent(getPopupText(e))
-        .openOn(map);
-
-    last_country_popup = e.target.feature.id;
+    return String(e.target.feature.properties.name)
 }
 
 function onEachFeature(feature, layer) {
@@ -58,25 +32,6 @@ function onEachFeature(feature, layer) {
         mouseover: highlight,
         click: highlight
     });
-}
-function resetHighlight(e) {
-    e.sourceTarget.setStyle({ fillOpacity: 0.8 });
-    popup.remove();
-    last_country_popup = null;
-}
-
-function style(feature) {
-    return {
-        fillColor: getColor(feature.id),
-        weight: 0.2,
-        color: "lightgrey",
-        fillOpacity: 0.8
-    };
-}
-
-
-async function fetchCountryData() {
-    return
 }
 
 function fetchGeoJson() {
@@ -94,58 +49,66 @@ function fetchGeoJson() {
             console.error("Error fetching GeoJSON:", error);
         });
 }
-function updateTimestampParagraph(timestamp) {
-    var timestamp_p = document.getElementById("timestamp_paragraph");
 
-
-    var dateObj = new Date(timestamp * 1000)
-
-    var options = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" };
-    timestamp_p.textContent += dateObj.toLocaleDateString("en-IE", options);
-
-}
 async function fillMap(response) {
-    geoJson = L.geoJson(response, { style: style(response) }).addTo(map)
-    geoJson.eachLayer(function (layer) {
-        onEachFeature(layer.feature, layer);
-    });
+    geoJson = L.geoJson(response, {
+        style: {
+            fillColor: `hsl(210, 100%, 20%)`,
+            weight: 0.2,
+            color: "lightgrey",
+            fillOpacity: 0.8
+        }
+    }).addTo(map)
 
-    const CountryJSON = await fetchCountryData();
-
-    max_value = Math.max(...Object.values(countryVisits));
     geoJson.eachLayer(function (layer) {
-        layer.setStyle(style(layer.feature));
+        layer.setStyle({
+            fillColor: `hsl(210, 100%, 20%)`,
+            weight: 0.2,
+            color: "lightgrey",
+            fillOpacity: 0.8
+        });
     });
 }
 fetchGeoJson();
 
-// const evtSource = new EventSource("//127.0.0.1:8080/events", {});
-// evtSource.onopen = function () {
-//     console.log('Connection to SSE server opened.');
-// };
-// evtSource.onerror = function (event) {
-//     console.log(event)
-// }
-//
-// // evtSource.onmessage = function (event) {
-// //     console.log(event)
-// // }
-// evtSource.addEventListener("special-event-name", function (event) {
-//     console.log("event listener:", event);
-// });
+function fadeOut(layer) {
+    let a = 70
+    let b = 20
+
+    let percent = 0
+    let interpolated = (b * percent) + (a * (1 - percent))
+
+    let delay = 15
+    let step = 0.01
+
+    let timer = setTimeout(function changeOpacity() {
+        if (percent < 1.0) {
+            interpolated = (b * percent) + (a * (1 - percent))
+            layer.setStyle({
+                fillColor: `hsl(210, 100%, ${interpolated}%)`
+            });
+            percent += step
+        }
+
+        timer = setTimeout(changeOpacity, delay);
+    }, delay)
+}
 function processEvent(event) {
     textArr = event.match(/.{1,3}/g);
     textArr.forEach(country => {
-        // console.log(country)
+        try {
+            geoJson.eachLayer(function (layer) {
+                if (layer.feature.id == country) {
+                    fadeOut(layer);
+                }
+            });
 
-        geoJson.eachLayer(function (layer) {
-            if (country == layer.feature.id) {
-                layer.options.fillColor = "black"
-                console.log(layer)
-            }
-        });
+        } catch (_) {
+
+        }
     });
 }
+
 fetch("//127.0.0.1:8080/events").then(response => {
     const reader = response.body.getReader();
 
