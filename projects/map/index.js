cdb32b8a57194f3cb419bd3b519bda79a7a9ee60
Hugo <114680022+hugorodriv@users.noreply.github.com>
2024-09-11 11:40:23 +0200

    styling. projects

--- projects/map/index.js
+++ projects/map/index.js
@@ -4,40 +4,41 @@ var popup;
 var last_country_popup;
 var geoJson;
 
-var map = L.map("map", { 
-    attributionControl:false, 
+var map = L.map("map", {
+    attributionControl: false,
     maxBoundsViscosity: 0.3,
     minZoom: 1.5,
     maxZoom: 5,
     autoPan: false,
     zoomSnap: 0.25,
-    maxBounds: [[-90,-180],   [90,180]],
-    keyboard:false}).setView([40, 0], 1.5);
+    maxBounds: [[-90, -180], [90, 180]],
+    keyboard: false
+}).setView([40, 0], 1.5);
 
-function getColor(id){
-    let a = 25
-    let b = 60
+function getColor(id) {
+    let a = 30
+    let b = 50
 
-    let percent = countryVisits[id]/max_value
+    let percent = countryVisits[id] / max_value
     let interpolated = (b * percent) + (a * (1 - percent))
 
-    interpolated = interpolated || 20
-    return `hsl(210, 100%, ${interpolated}%)`;
+    interpolated = interpolated || 25 // substitute null for 25 (would end up being a black color)
+    return `hsl(185, 80%, ${interpolated}%)`;
 }
 
-function getPopupText(e){
+function getPopupText(e) {
     var num = countryVisits[e.target.feature.id];
-    if (num === undefined){
+    if (num === undefined) {
         num = 0;
     }
     return String(e.target.feature.properties.name + " : " + num)
 }
 
-function highlight(e){
+function highlight(e) {
     var layer = e.target;
     popup = L.popup({
         closeButton: false,
-        autoPan: false        
+        autoPan: false
     })
         .setLatLng(e.latlng)
         .setContent(getPopupText(e))
@@ -47,7 +48,7 @@ function highlight(e){
 }
 
 function onEachFeature(feature, layer) {
-    if("ATA" === layer.feature.id){     //remove antartica
+    if ("ATA" === layer.feature.id) {     //remove antartica
         layer.remove();
     }
     layer.on({
@@ -57,7 +58,7 @@ function onEachFeature(feature, layer) {
     });
 }
 function resetHighlight(e) {
-    e.sourceTarget.setStyle({fillOpacity:0.8});
+    e.sourceTarget.setStyle({ fillOpacity: 0.8 });
     popup.remove();
     last_country_popup = null;
 }
@@ -74,51 +75,51 @@ function style(feature) {
 
 async function fetchCountryData() {
     return await fetch("countries.json", {
-            method: "GET",
-            headers: {
-                "Accept": "application/json",
-            },
-        })
-        .then((response)=>response.json())
-        .then((responseJson)=>{return responseJson});
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then((responseJson) => { return responseJson });
 }
 
-function fetchGeoJson(){
+function fetchGeoJson() {
     fetch("https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json", {
         method: "GET",
         headers: {
             "Accept": "application/json",
         },
     })
-    .then(response => response.json())
-    .then(data => {
-        fillMap(data);
-    })
-    .catch(error => {
-        console.error("Error fetching GeoJSON:", error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            fillMap(data);
+        })
+        .catch(error => {
+            console.error("Error fetching GeoJSON:", error);
+        });
 }
-function updateTimestampParagraph(timestamp){
+function updateTimestampParagraph(timestamp) {
     var timestamp_p = document.getElementById("timestamp_paragraph");
 
-    
-    var dateObj = new Date(timestamp*1000)
 
-    var options = {year: "numeric", month: "long", day: "numeric", hour:"numeric", minute:"numeric"};
+    var dateObj = new Date(timestamp * 1000)
+
+    var options = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" };
     timestamp_p.textContent += dateObj.toLocaleDateString("en-IE", options);
 
 }
-async function fillMap(response){
-    geoJson = L.geoJson(response, {style: style(response)}).addTo(map)
+async function fillMap(response) {
+    geoJson = L.geoJson(response, { style: style(response) }).addTo(map)
     geoJson.eachLayer(function (layer) {
         onEachFeature(layer.feature, layer);
     });
 
     const CountryJSON = await fetchCountryData();
-    
+
     countryVisits = CountryJSON["Countries"];
     updateTimestampParagraph(CountryJSON["Timestamp"]);
-    
+
 
     max_value = Math.max(...Object.values(countryVisits));
     geoJson.eachLayer(function (layer) {
